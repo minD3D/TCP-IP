159a02153d816b5e90cf9df1ce74ad933739034a
[minD3D] <gomdori626@gmail.com>
2017-11-10 11:14:44 +0900

    serv code added

--- echo_selectserv.c
+++ echo_selectserv.c
@@ -1,9 +1,12 @@
 /*
+fd_set[]의 번호
 
 connected client: 4
 connected client: 5
-
 이렇게 뜨는데 3은 서버소켓이기 때문!
+0 표준입력
+1 표준출력
+2 에러
 */
 
 #include <stdio.h>
@@ -55,12 +58,13 @@ int main(int argc, char *argv[]){
         timeout.tv_sec=5;
         timeout.tv_usec=5000;
 
-        if((fd_num=select(fd_max+1,&cpy_reads,0,0,&timeout))==-1)
+        // fd_max+1은 갯수를 넣어야하기 때문에 파일디스크립터 최대값 +1 이 총 갯수        
+        if((fd_num=select(fd_max+1,&cpy_reads,0,0,&timeout))==-1)//select는 변경된 값의 갯수를 리턴한다
             break;
 
-        for(i=0; i<fd_max+1; i++){
+        for(i=0; i<fd_max+1; i++){//모든 파일 디스크립터 안의 값이 1인지 확인한다.[0/0/0/0/1]로 셋되어 있을것.
             if(FD_ISSET(i,&cpy_reads)){
-                if(i==serv_sock){
+                if(i==serv_sock){//서버소켓 위치인 경우
                     adr_sz=sizeof(clnt_adr);
                     clnt_sock=accept(serv_sock,(struct sockaddr*)&clnt_adr,&adr_sz);
                 FD_SET(clnt_sock,&reads);
@@ -69,7 +73,7 @@ int main(int argc, char *argv[]){
                 
                 printf("connected client: %d \n", clnt_sock);
                 }
-                else{
+                else{//클라이언트 소켓 파일디스크립터가 1인경우
                     str_len=read(i,buf,BUF_SIZE);
                     if(str_len==0){
                         FD_CLR(i,&reads);
